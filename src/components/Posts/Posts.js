c5420b86acb3e71aa00d8d7c3473e7deb61d4d2e
Erica Girges <ericafiorenza@gmail.com>
2020-08-11 13:27:02 -0700

    App.js and Posts.js completed

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -9,7 +9,13 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map( i => {
+        const post = i
+        console.log(post)
+        return <div className="post" key={post.call}>{post.call}</div>
+        })}
       {/* Check the implementation of Post to see what props it requires! */}
+      <Post likePost={likePost} />
     </div>
   );
 };
